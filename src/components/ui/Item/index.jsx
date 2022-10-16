c120901b26e92743a3ddeb3bf98f9b50d674449e
CoderNazar <nazardubovik383@gmail.com>
2022-10-17 02:18:48 +0300

    chore: create component ui Item

--- src/components/ui/Item/index.jsx
+++ src/components/ui/Item/index.jsx
@@ -1,24 +1,60 @@
 import React from 'react';
+import cx from 'classnames';
 import PropTypes from 'prop-types';
+import styles from './index.module.css';
 
-const Item = ({ label, quantity }) => {
+const ContentItem = ({
+  statLabel,
+  statInfo,
+  classNameLabel,
+  classNameInfo,
+}) => {
   return (
-    <li>
-      <span>{label}</span>
-      <span>{quantity}</span>
+    <>
+      <span className={classNameLabel}>{statLabel}</span>
+      <span className={classNameInfo}>{statInfo}</span>
+    </>
+  );
+};
+
+const Item = ({
+  children,
+  statLabel,
+  statInfo,
+  className,
+  classNameLabel,
+  classNameInfo,
+}) => {
+  return (
+    <li className={cx(styles.containerItem, className)}>
+      {children ? children : (
+        <ContentItem
+          statLabel={statLabel}
+          statInfo={statInfo}
+          classNameLabel={classNameLabel}
+          classNameInfo={classNameInfo}
+        />
+      )}
     </li>
   );
 };
 
 Item.defaultProps = {
-  label: 'Label',
-  quantity: 0,
+  children: null,
+  statLabel: 'label',
+  statInfo: 'info',
+  className: '',
+  classNameLabel: '',
+  classNameInfo: '',
 };
 
 Item.propTypes = {
-  label: PropTypes.string,
-  quantity: PropTypes.number,
+  children: PropTypes.node,
+  statLabel: PropTypes.string,
+  statInfo: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  className: PropTypes.string,
+  classNameLabel: PropTypes.string,
+  classNameInfo: PropTypes.string,
 };
-
 
 export { Item };
