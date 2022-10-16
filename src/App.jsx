5739d59a756cc3481958ecdcc1ad0a013dab019a
CoderNazar <nazardubovik383@gmail.com>
2022-10-17 02:20:20 +0300

    chore: add func in App for get data for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { getDataUser } from 'services';
-import { Profile, Loader } from 'components/shared';
+import {
+  getDataUser,
+  getStatisticsData,
+  getFriendList,
+  getTransactionsHistory,
+} from 'services';
+import {
+  Profile,
+  Loader,
+  Statistics,
+  FriendList,
+  TransactionHistory,
+} from 'components/shared';
 import { isEmpty } from 'utils/helpers';
 
 const App = () => {
   const [user, setUser] = useState({});
-  const { stats, ...dataUser } = user;
+  const [data, setData] = useState([]);
+  const [friendList, setFriendList] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+
+  const { stats, username, tag, location, avatar } = user;
   const isLoader = isEmpty(user);
 
-  const dataRetrievalRequest = async () => {
+  const requestRetrieveUserData = async () => {
     const user = await getDataUser();
     setUser(user);
   };
 
+  const requestRetrieveStatisticsData = async () => {
+    const data = await getStatisticsData();
+    setData(data);
+  };
+
+  const requestRetrieveFriendList = async () => {
+    const friendList = await getFriendList();
+    setFriendList(friendList);
+  };
+
+  const requestRetrieveTransactions = async () => {
+    const transactions = await getTransactionsHistory();
+    setTransactions(transactions);
+  };
+
   useEffect(() => {
-    dataRetrievalRequest();
+    requestRetrieveUserData();
+    requestRetrieveStatisticsData();
+    requestRetrieveFriendList();
+    requestRetrieveTransactions();
   }, []);
 
   return (
-    <>
-      {isLoader ? (
-        <Loader />
-      ) : (
-        <Profile
-          stats={stats}
-          {...dataUser}
-        />
-      )}
-    </>
+    <>{isLoader ? <Loader /> : <TransactionHistory items={transactions} />}</>
   );
 };
 
