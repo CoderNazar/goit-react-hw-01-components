73d90b8339ac4c81048a1bb486f37f3552bd9d21
CoderNazar <nazardubovik383@gmail.com>
2022-10-17 02:18:31 +0300

    chore: create component ui Heading

--- src/components/ui/Heading/index.jsx
+++ src/components/ui/Heading/index.jsx
@@ -2,14 +2,14 @@ import React, { createElement } from 'react';
 import PropTypes from 'prop-types';
 
 const Heading = ({ tag, text, className }) => {
-  const Component = () => createElement(tag);
-  return <Component className={className}>{text}</Component>;
+  const Component = () => createElement(tag, { className }, text);
+  return <Component/>;
 };
 
 Heading.defaultProps = {
   tag: 'h1',
   text: 'text',
-  className: 'name',
+  className: '',
 };
 
 Heading.propTypes = {
