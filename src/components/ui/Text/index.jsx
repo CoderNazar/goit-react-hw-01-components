32c57d354d5a4ac676f51a6adc86c7d8f55cb23c
CoderNazar <nazardubovik383@gmail.com>
2022-10-17 02:19:32 +0300

    chore: create component ui Text

--- src/components/ui/Text/index.jsx
+++ src/components/ui/Text/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{memo} from 'react';
 import PropTypes from 'prop-types';
 
-const Text = ({ text, className }) => {
+const Text = memo(({ text, className }) => {
   return <p className={className}>{text}</p>;
+});
+
+Text.defaultProps = {
+  className: '',
 };
 
 Text.propTypes = {
   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export { Text };
